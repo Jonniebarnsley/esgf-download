db9e7930a7020d8ac7c09ed4a91535bda8e76056
Jonnie Barnsley <jonniebarnsley@gmail.com>
2025-07-16 20:08:17 +0100

    limit rows in console to 40 (by default)

--- esgf_download/console.py
+++ esgf_download/console.py
@@ -5,4 +5,6 @@ Shared rich console instance for consistent formatting throughout the package.
 from rich.console import Console
 
 # Shared rich console instance for consistent formatting
-console = Console() 
+console = Console()
+
+MAX_DISPLAY_ROWS = 40 # max number of rows to display in progress bar
